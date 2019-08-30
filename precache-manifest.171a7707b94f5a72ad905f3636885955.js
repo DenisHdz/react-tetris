f4d99289adf7bde3f6d851cb48ba8c8954c94d36
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-08-30 21:11:52 +0100

    Updates

---
+++ precache-manifest.171a7707b94f5a72ad905f3636885955.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b070b42b8acd0785901abe4401a2e812",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "45ef4320a2314fafc18d",
+    "url": "/react-tetris/static/css/main.7e2c0af0.chunk.css"
+  },
+  {
+    "revision": "51007ef1709d3dc96a04",
+    "url": "/react-tetris/static/js/2.f2097705.chunk.js"
+  },
+  {
+    "revision": "45ef4320a2314fafc18d",
+    "url": "/react-tetris/static/js/main.024b90c8.chunk.js"
+  },
+  {
+    "revision": "3cbf365fbc4eeca393c0",
+    "url": "/react-tetris/static/js/runtime~main.1a1357f3.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "74496d9086d97aaeeafb3085e9957668",
+    "url": "/react-tetris/static/media/PressStart2P.74496d90.ttf"
+  },
+  {
+    "revision": "93e70a2a4c23aa356f5258c181754d2a",
+    "url": "/react-tetris/static/media/bg.93e70a2a.jpg"
+  }
+]);
